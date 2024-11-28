4ce7922255c972babf0b5d9cd5332469c1f8c4a7
Aneesh Rao <sidrao2006@gmail.com>
2024-11-28 10:23:42 +0530

    Use env variables for db config

--- backend-cal/cal_engine/settings.py
+++ backend-cal/cal_engine/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': 'db',
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
