784968757b5a21967030a7cbd36bc2dc2ca68f2c
Aditya <adityaburgula123@gmail.com>
2024-12-02 06:35:57 +0000

    Update Question model

--- backend-cal/cal_engine/assessment/urls.py
+++ backend-cal/cal_engine/assessment/urls.py
@@ -1,10 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from .views import AssessmentViewSet, QuestionViewSet, QuestionOptionViewSet, AssessmentGradingViewSet
+from .views import AssessmentViewSet, QuestionViewSet
 
 router = DefaultRouter()
 router.register(r'assessments', AssessmentViewSet)
 router.register(r'questions', QuestionViewSet)
-router.register(r'question-options', QuestionOptionViewSet)
-router.register(r'assessment-grading', AssessmentGradingViewSet)
 
 urlpatterns = router.urls
