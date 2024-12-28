1d69a5123a0a39f471c69f3d124332779af826ad
Kapil-Mulay-1421 <krmulay@gmail.com>
2024-12-28 09:56:25 +0530

    upgrade to node version 22.12.0

--- frontend-cal/tailwind.config.js
+++ frontend-cal/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import tailwindcssAnimate from 'tailwindcss-animate';
+export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
@@ -63,5 +64,5 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
