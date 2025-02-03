140ebaf239e43b44f6a96592b26e0e66458d0b7b
Aditya <adityaburgula123@gmail.com>
2025-02-03 19:03:19 +0530

    Fixed the setup related bugs

--- backend/lms_engine/core/settings.py
+++ backend/lms_engine/core/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+
+# Ensure the logs directory exists
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
