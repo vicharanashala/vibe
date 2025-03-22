a6f5e06e1d32335f0ed81ba9c114a8ee82435dc2
Aditya <adityaburgula123@gmail.com>
2025-03-22 17:16:29 +0530

    fix(tests): update Jest configuration to resolve shared and modules paths, and comment out unused path mappings in tsconfig

--- backend-proctoring/main/jest.config.js
+++ backend-proctoring/main/jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
     setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
     moduleNameMapper: {
       "^@/(.*)$": "<rootDir>/src/$1",
+      '^shared/(.*)$': '<rootDir>/src/shared/$1',  // Resolve shared path in Jest
+      '^modules/(.*)$': '<rootDir>/src/modules/$1' // Resolve modules path if used
+
     },
   };
   
