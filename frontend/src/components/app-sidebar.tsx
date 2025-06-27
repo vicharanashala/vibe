ec13286e1e21549ec224b0b6a7d1e1582e034009
Saaransh Garg <68538240+saaranshgarg1@users.noreply.github.com>
2025-06-27 17:16:55 +0530

    Update app-sidebar.tsx

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -48,7 +48,7 @@ export function AppSidebar() {
         url: "#",
         icon: BookOpen,
         items: [
-          { title: "Add Course", url: "/teacher/add-course" },
+          { title: "Add Course", url: "/teacher/course/create" },
           { title: "View Course", url: "/teacher/courses/get" },
           { title: "List Courses", url: "/teacher/courses/list" },
         ],
