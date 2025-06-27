f391aff3c081ae88b878b33fe7c0634b9032b268
Srivatsa180717 <vatsaa99@gmail.com>
2025-06-27 14:58:02 +0530

    Upgraded AddCourse Page and connected it to the backend

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -48,7 +48,7 @@ export function AppSidebar() {
         url: "#",
         icon: BookOpen,
         items: [
-          { title: "Add Course", url: "/teacher/courses/create" },
+          { title: "Add Course", url: "/teacher/add-course" },
           { title: "View Course", url: "/teacher/courses/get" },
           { title: "List Courses", url: "/teacher/courses/list" },
         ],
