8e7a6aca226d5909629a2740e6cc6eb34c1d16f8
Aditya <58661255+adityabmv@users.noreply.github.com>
2025-07-19 21:26:41 +0530

    Update start.sh

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -2,7 +2,6 @@
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
 /app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=gcp
-/app/tailscale status
 
 curl http://100.100.108.11:8017/
 
