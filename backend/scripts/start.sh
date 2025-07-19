e24a08381a9e6500ad24a7b6eb95436c6c56faf8
Aditya <58661255+adityabmv@users.noreply.github.com>
2025-07-19 21:16:05 +0530

    Update start.sh

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -4,6 +4,8 @@
 /app/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=gcp
 /app/tailscale status
 
+curl http://100.100.108.11:8017/
+
 ALL_PROXY=socks5://localhost:1055/
 
 dumb-init node build/index.js
