a15d68422a64cbf9179840ea439329339857f97b
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-07-25 13:33:10 +0530

    merged changes in reportAbilities

--- backend/src/modules/reports/abilities/reportsAbilities.ts
+++ backend/src/modules/reports/abilities/reportsAbilities.ts
@@ -4,10 +4,9 @@ import {
   ICourse,
   IReport,
 } from '#root/shared/index.js';
-import {AbilityBuilder, MongoAbility} from '@casl/ability';
-import {ReportPermissionSubject} from '../constants.js';
-import {createAbilityBuilder} from '#root/modules/notifications/abilities/types.js';
-import {ObjectId} from 'mongodb';
+import { AbilityBuilder, MongoAbility } from '@casl/ability';
+import { ReportPermissionSubject } from '../constants.js';
+import { createAbilityBuilder } from '#root/modules/notifications/abilities/types.js';
 
 export enum ReportsActions {
   Manage = 'manage',
@@ -21,7 +20,7 @@ export async function setupReportsAbilities(
   builder: AbilityBuilder<any>,
   user: AuthenticatedUser,
 ) {
-  const {can, cannot} = builder;
+  const { can, cannot } = builder;
 
   if (user.globalRole == 'admin') {
     can(ReportsActions.Manage, ReportPermissionSubject.REPORT);
@@ -30,55 +29,45 @@ export async function setupReportsAbilities(
 
   // It will ensure the student can only report on enrolled course
   user.enrollments.forEach((enrollment: AuthenticatedUserEnrollements) => {
-    if (enrollment.role === 'STUDENT') {
-      can(ReportsActions.Create, ReportPermissionSubject.REPORT, {
-        courseId: enrollment.courseId,
-        courseVersionId: enrollment.versionId,
-        reportedBy: user.userId,
-      });
+    const userBounded: Partial<IReport> = {
+      reportedBy: user.userId,
+    };
+
+    const reportBounded: Partial<IReport> = {
+      courseId: enrollment.courseId,
+      versionId: enrollment.versionId,
+    };
+
+    switch (enrollment.role) {
+      case 'STUDENT':
+        can(ReportsActions.Create, ReportPermissionSubject.REPORT, {
+          ...reportBounded,
+          ...userBounded,
+        });
+        break;
+      case 'INSTRUCTOR':
+        can(ReportsActions.View, ReportPermissionSubject.REPORT);
+        can(
+          ReportsActions.Modify,
+          ReportPermissionSubject.REPORT,
+          reportBounded,
+        );
+        // cannot(
+        //   ReportsActions.Delete,
+        //   ReportPermissionSubject.REPORT,
+        //   reportBounded,
+        // );
+        break;
+      case 'MANAGER':
+        can(ReportsActions.View, ReportPermissionSubject.REPORT);
+      // cannot(ReportsActions.Delete, ReportPermissionSubject.REPORT, 
+      //   reportBounded,
+      // );
+      case 'TA':
+        can(ReportsActions.View, ReportPermissionSubject.REPORT);
+        break;
     }
-  });
-
-  // I am considering that, in the instructor dashboard, we should display only those reports that belong to the courses instructed by the respective instructor
-
-  const isInstructor = user.enrollments.some(e => e.role === 'INSTRUCTOR');
-  const isManager = user.enrollments.some(e => e.role === 'MANAGER');
-
-  if (isInstructor || isManager) {
-    const instructorId = user.userId;
-
-    const instructorCourses = []; // Need to fetch this from db
-
-    const courseIds = instructorCourses.map((c: ICourse) => c._id.toString());
-
-    if (isInstructor) {
-      can(ReportsActions.View, ReportPermissionSubject.REPORT, {
-        courseId: {$in: courseIds},
-      });
-
-      can(ReportsActions.Modify, ReportPermissionSubject.REPORT, {
-        courseId: {$in: courseIds},
-      });
-
-      cannot(ReportsActions.Delete, ReportPermissionSubject.REPORT, {
-        courseId: {$in: courseIds},
-      });
-    }
-
-    if (isManager) {
-      can(ReportsActions.View, ReportPermissionSubject.REPORT, {
-        courseId: {$in: courseIds},
-      });
-
-      can(ReportsActions.Modify, ReportPermissionSubject.REPORT, {
-        courseId: {$in: courseIds},
-      });
-
-      cannot(ReportsActions.Delete, ReportPermissionSubject.REPORT, {
-        courseId: {$in: courseIds},
-      });
-    }
-  }
+  })
 }
 
 // Get report ablities for a user - ready to use in controller
