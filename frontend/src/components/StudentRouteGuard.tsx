ed0fbdb72eb62041c22a24ddd74c56bb7170bae7
Abiram-k <abiramk0107@gmail.com>
2025-08-05 16:12:24 +0530

    fix(student): Mobile block

--- frontend/src/components/StudentRouteGuard.tsx
+++ frontend/src/components/StudentRouteGuard.tsx
@@ -2,13 +2,17 @@
 import React from "react";
 import { isMobile } from "react-device-detect";
 import MobileFallbackScreen from "./MobileFallbackScreen";
+import { useRouterState } from "@tanstack/react-router";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const StudentRouteGuard: React.FC<Props> = ({ children }) => {
-  if (isMobile) {
+  const location = useRouterState({ select: s => s.location });
+
+  const isStudentPath = location.pathname.startsWith("/student");
+  if (isMobile && isStudentPath) {
     return <MobileFallbackScreen/>;
   }
   return <>{children}</>;
