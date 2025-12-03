c4dc8df459da2aff95cf86ce882663c83e927b9c
mamatha12-reddy <mamathareddy3456.reddeppa@gmail.com>
2025-12-03 10:10:35 +0530

    Added body parser in index.ts file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -20,6 +20,9 @@ import { authorizationChecker } from './shared/functions/authorizationChecker.js
 import { currentUserChecker } from './shared/functions/currentUserChecker.js';
 
 const app = express();
+app.use(express.json());                       
+app.use(express.urlencoded({ extended: true })); 
+
 
 app.use(loggingHandler);
 // app.use(
