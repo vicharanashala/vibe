6669f66b4cf2aa3ed907b4b0b410a497adb77bc9
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-12-25 09:42:14 +0530

    Bulk update progress cron added

--- backend/src/utils/startCron.ts
+++ backend/src/utils/startCron.ts
@@ -1,11 +1,12 @@
-import {getFromContainer} from 'routing-controllers';
-import {DeleteCronService} from '#root/modules/courses/services/deleteCronService.js';
+import { getFromContainer } from 'routing-controllers';
+import { DeleteCronService } from '#root/modules/courses/services/deleteCronService.js';
 
 export const startCron = () => {
   try {
     // Get DeleteCronService from the existing container and schedule it
     const deleteCronService = getFromContainer(DeleteCronService);
     deleteCronService.scheduleDeleteCron();
+    deleteCronService.scheduleBulkUpdateProgress();
 
     console.log('✅ Delete cron job scheduled successfully');
   } catch (error) {
