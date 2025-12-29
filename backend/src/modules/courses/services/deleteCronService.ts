26f820ced78e2dda13f8a4e8157b395950444745
Abiram-k <Abiramk0107@gmail.com>
2025-12-29 10:27:17 +0530

    feat: add bulk update watchtime in cron job

--- backend/src/modules/courses/services/deleteCronService.ts
+++ backend/src/modules/courses/services/deleteCronService.ts
@@ -1,14 +1,15 @@
 import cron from 'node-cron';
-import { injectable, inject } from 'inversify';
+import {injectable, inject} from 'inversify';
 import {
   BaseService,
   CourseRepository,
   MongoDatabase,
 } from '#root/shared/index.js';
-import { GLOBAL_TYPES } from '#root/types.js';
-import { USERS_TYPES } from '#root/modules/users/types.js';
-import { ItemRepository } from '#root/shared/database/providers/mongo/repositories/ItemRepository.js';
-import { EnrollmentService } from '#root/modules/users/services/EnrollmentService.js';
+import {GLOBAL_TYPES} from '#root/types.js';
+import {USERS_TYPES} from '#root/modules/users/types.js';
+import {ItemRepository} from '#root/shared/database/providers/mongo/repositories/ItemRepository.js';
+import {EnrollmentService} from '#root/modules/users/services/EnrollmentService.js';
+import {ProgressService} from '#root/modules/users/services/ProgressService.js';
 
 /**
  * Clean up Versions, modules, Sections, ItemsGroup, items.
@@ -22,6 +23,8 @@ export class DeleteCronService extends BaseService {
     private readonly itemRepo: ItemRepository,
     @inject(USERS_TYPES.EnrollmentService)
     private readonly enrollmentService: EnrollmentService,
+    @inject(USERS_TYPES.ProgressService)
+    private readonly progressService: ProgressService,
 
     @inject(GLOBAL_TYPES.Database)
     private readonly mongoDatabase: MongoDatabase,
@@ -51,8 +54,14 @@ export class DeleteCronService extends BaseService {
 
       // Execute the cleanup within a transaction
       await this._withTransaction(async session => {
+        console.log('Step 1: Creating watchTime docs');
+        await this.progressService.createBulkWatchiTimeDocs(
+          '6943b2cafa4e840eb39490b6',
+          '6943b2cafa4e840eb39490b7',
+        );
+        console.log('Step 2: Updating enrollment progress');
         await this.enrollmentService.bulkUpdateAllEnrollments(
-          "6943b2cafa4e840eb39490b6"
+          '6943b2cafa4e840eb39490b6',
         );
         console.log('Bulk update cron job completed successfully');
       });
@@ -60,5 +69,4 @@ export class DeleteCronService extends BaseService {
 
     console.log('Bulk update cron job scheduled to run every hour');
   }
-
 }
