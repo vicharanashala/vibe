9828ef95dee4f303e2429e93cacdb74200704c91
Rishav Kumar <152714834+rishavkumarnit@users.noreply.github.com>
2026-01-09 12:56:43 +0530

    add custom ui for rjsf form for student registration form (#621)
    * added custom ui for rjsf form for student registration
    
    * minor modification
    
    * make dialog box fit according to Form items

---
+++ frontend/src/app/pages/student/components/CustomSubmitButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export function CustomSubmitButton() {
+
+  return (
+    <div className="ml-1 mt-6 mb-6 flex justify-center">
+      <Button type="submit" variant="default" size="default">Submit</Button>
+    </div>
+  );
+}
