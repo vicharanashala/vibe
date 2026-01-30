b811270f469453dd5ef2c05751a4f5a268cce97f
Pushan Sinha <61679844+programm3d@users.noreply.github.com>
2026-01-30 10:01:03 +0530

    Fix Dynamic Content Title Display (#679)
    feature/dynamic-content-title

--- backend/src/modules/courses/classes/validators/ItemValidators.ts
+++ backend/src/modules/courses/classes/validators/ItemValidators.ts
@@ -696,7 +696,7 @@ class ItemRefResponse implements ItemRef {
   _id?: ID;
 
   @JSONSchema({
-    description: 'The name of the item',
+    description: 'The type of the item',
     type: 'string',
     readOnly: true,
   })
@@ -712,6 +712,24 @@ class ItemRefResponse implements ItemRef {
   @IsNotEmpty()
   @IsString()
   order: string;
+
+  @JSONSchema({
+    description: 'Whether the item is hidden',
+    type: 'boolean',
+    readOnly: true,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isHidden?: boolean;
+
+  @JSONSchema({
+    description: 'The name of the item',
+    type: 'string',
+    readOnly: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
 }
 
 class ItemsGroupResponse implements ItemsGroup {
