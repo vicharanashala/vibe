7107bc39867841be2055f6dead5a385ea56ef38d
Thirumalesh921 <thirumalesnemalipuri@outlook.com>
2026-02-05 12:02:00 +0530

    Removed un-used import

--- frontend/src/components/StudentRouteGuard.tsx
+++ frontend/src/components/StudentRouteGuard.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { isMobile } from "react-device-detect";
 import MobileFallbackScreen from "./MobileFallbackScreen";
-import { useRouterState } from "@tanstack/react-router";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const StudentRouteGuard: React.FC<Props> = ({ children }) => {
-  
+
   if (isMobile) {
     return <MobileFallbackScreen/>;
   }
