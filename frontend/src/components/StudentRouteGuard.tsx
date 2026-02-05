fe5a0cc7d29783214dd7681185da7a9c6c536687
Thirumalesh921 <thirumalesnemalipuri@outlook.com>
2026-02-05 12:22:33 +0530

    Mobile fallback screen updated. (#719)
    * Added protected route for course page
    
    * Added before load
    
    * Removed un-used import

--- frontend/src/components/StudentRouteGuard.tsx
+++ frontend/src/components/StudentRouteGuard.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { isMobile } from "react-device-detect";
 import MobileFallbackScreen from "./MobileFallbackScreen";
-import { useRouterState } from "@tanstack/react-router";
 
 interface Props {
   children: React.ReactNode;
@@ -9,11 +8,7 @@ interface Props {
 
 const StudentRouteGuard: React.FC<Props> = ({ children }) => {
 
-  const location = useRouterState({ select: state => state.location.pathname });
-
-  const isStudentRoute = location.startsWith("/student");
-  
-  if (isMobile && isStudentRoute) {
+  if (isMobile) {
     return <MobileFallbackScreen/>;
   }
   return <>{children}</>;
