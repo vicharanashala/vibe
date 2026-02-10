55cb380609381d474d49c2318b7e00de9ee55298
Rishabh Shukla <shuklarishabh890@gmail.com>
2026-02-10 09:30:35 +0530

    implemented the tranformer of audit_trails.

--- backend/src/shared/database/providers/mongo/repositories/ItemRepository.ts
+++ backend/src/shared/database/providers/mongo/repositories/ItemRepository.ts
@@ -17,6 +17,7 @@ import {
   Item,
   FeedBackFormItem,
   ItemRef,
+  AuditTrail,
 } from '#courses/classes/transformers/Item.js';
 import { UpdateItemBody } from '#root/modules/courses/classes/index.js';
 import { QuestionBank } from '#root/modules/quizzes/classes/transformers/QuestionBank.js';
@@ -33,6 +34,7 @@ export class ItemRepository implements IItemRepository {
   private questionBankCollection: Collection<QuestionBank>;
   private questionsCollection: Collection<any>;
   private courseVersionCollection: Collection<any>;
+  // private auditCollection: Collection<AuditTrail>;
 
   constructor(
     @inject(GLOBAL_TYPES.Database)
@@ -63,6 +65,9 @@ export class ItemRepository implements IItemRepository {
     this.courseVersionCollection = await this.db.getCollection<CourseVersion>(
       'newCourseVersion',
     );
+    // this.auditCollection = await this.db.getCollection<AuditTrail>(
+    //   'auditTrail',
+    // );
   }
 
   // Methods for ItemsGroup operations
@@ -253,7 +258,8 @@ export class ItemRepository implements IItemRepository {
   // Methods for Item CRUD operations
   async createItem(item: Item, session?: ClientSession): Promise<Item | null> {
     await this.init();
-
+    // const auditTrail = new AuditTrail(item._id, 'create');
+    // await this.auditCollection.insertOne(auditTrail, { session });
     let collection: Collection<any> = null;
     switch (item.type) {
       case ItemType.VIDEO:
