65bfbf8d2d19346dd651d05fa54099e33b7aa74c
Abiram-k <abiramk0107@gmail.com>
2026-03-08 11:29:07 +0530

    feat(backend): add auth in ledger listing

--- backend/src/modules/hpSystem/classes/validators/ledgerValidators.ts
+++ backend/src/modules/hpSystem/classes/validators/ledgerValidators.ts
@@ -1,11 +1,31 @@
 import { Expose, Type } from "class-transformer";
 import { HpLedgerTransformer } from "../transformers/Ledger.js";
+import { IsNumber, IsString, ValidateNested } from "class-validator";
+
+export class StudentLedgerDetailsDto {
+    @Expose()
+    @IsString()
+    studentName: string;
+
+    @Expose()
+    @IsString()
+    studentEmail: string;
+
+    @Expose()
+    @IsNumber()
+    hpPoints: number;
+}
 
 export class LedgerListResponseDto {
 
     @Expose()
     @Type(() => HpLedgerTransformer)
     data: HpLedgerTransformer[];
+
+    @Expose()
+    @ValidateNested()
+    @Type(() => StudentLedgerDetailsDto)
+    studentDetails: StudentLedgerDetailsDto;
 
     @Expose()
     total: number;
