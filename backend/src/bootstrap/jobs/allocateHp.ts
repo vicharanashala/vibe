8ed7cf4c3a9fa13b639c5cd58e1048e487b38d6d
Abiram-k <abiramk0107@gmail.com>
2026-03-22 17:55:40 +0530

    fix(frontend): hide activity submit button for milestone activities

--- backend/src/bootstrap/jobs/allocateHp.ts
+++ backend/src/bootstrap/jobs/allocateHp.ts
@@ -9,15 +9,15 @@ cron.schedule(
     '*/5 * * * *',
     // "*/5 * * * * *",
     async () => {
- 
+
         try {
             const ENABLE_HP_JOB = appConfig.ENABLE_HP_JOB;
             if (ENABLE_HP_JOB) {
                 console.log('Cron job started for hp system checks...');
                 try {
                     console.log('⚡ Running penalty allocation job...');
-                    await allocatePenalty(); 
-                    console.log('✅ Penalty allocation job completed'); 
+                    await allocatePenalty();
+                    console.log('✅ Penalty allocation job completed');
 
                     console.log('🎯 About to run milestone reward allocation job...');
                     await allocateReward();
