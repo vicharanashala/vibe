6ff6197fb5539a77a8a9346532c95c63580cfe2c
programm3d <pushansinha8@gmail.com>
2026-06-09 18:52:37 +0530

    fix(backend): replace global body parser with per-route body parser to prevent stream is not readable error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,8 +25,6 @@ const globalRateLimiter = createRateLimiter();
 
 // app.use(globalRateLimiter);
 app.use(loggingHandler);
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.set('trust proxy', 1);
 
